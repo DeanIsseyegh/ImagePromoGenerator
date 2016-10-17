a78581826c1f2aa73d26ab5f6908fc24f1a00a41
Dean Isseyegh <dean.isseyegh@secretescapes.com>
2016-10-17 12:09:00 +0100

    Implemented first promo containing box class

--- image-promo-generator/src/groovy/image/promo/generator/PromoContainerBox.groovy
+++ image-promo-generator/src/groovy/image/promo/generator/PromoContainerBox.groovy
@@ -1,12 +1,22 @@
-package overlay.app
+package image.promo.generator
 
+import org.xhtmlrenderer.css.parser.property.PrimitivePropertyBuilders.Height
+
+import javax.imageio.ImageIO
+import java.awt.Color
+import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 
 class PromoContainerBox {
 
 	String hexColour
 	String promoText
-	BufferedImage image
+
+	BufferedImage generatedImage
+
+	public static final MIN_WIDTH = 35
+	public static final WIDTH = 130
+	public static final HEIGHT = 28
 
 	public PromoContainerBox(String hexColour, String promoText) {
 		this.hexColour = hexColour
@@ -14,8 +24,33 @@ class PromoContainerBox {
 		generatePromoBox()
 	}
 
-	BufferedImage generatePromoBox() {
-
+	BufferedImage getImage() {
+		generatedImage
 	}
 
+	private generatePromoBox() {
+		BufferedImage bufferedImage = createBufferedImage()
+		drawRectangeOnImage(bufferedImage)
+		generatedImage = bufferedImage
+	}
+
+	private drawRectangeOnImage(BufferedImage bufferedImage) {
+		Graphics2D g2d = bufferedImage.createGraphics()
+		g2d.setColor(generateColorBasedOnHex())
+		g2d.fillRect(0, 0, WIDTH, HEIGHT)
+		g2d.dispose()
+	}
+
+	private createBufferedImage() {
+		new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB)
+	}
+
+	private Color generateColorBasedOnHex() {
+		Color.decode(hexColour)
+	}
+
+/*	public static testSave(img) {
+		File outputfile = new File("rectestx.png");
+		ImageIO.write(img, "png", outputfile);
+	}*/
 }
--- image-promo-generator/test/unit/image/promo/generator/PromoContainerBoxUnitSpec.groovy
+++ image-promo-generator/test/unit/image/promo/generator/PromoContainerBoxUnitSpec.groovy
@@ -1,7 +1,60 @@
 package image.promo.generator
 
-/**
- * Created by dean on 17/10/16.
- */
-class PromoContainerBoxUnitSpec {
+import spock.lang.Specification
+
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+class PromoContainerBoxUnitSpec extends Specification {
+
+	final String orangeHexColour = "#ff8c00"
+
+	PromoContainerBox promoContainerBox
+
+	def setup() {
+		promoContainerBox = new PromoContainerBox(orangeHexColour, "testpromo")
+	}
+
+	def "Generates color based on hex"() {
+		when:
+			Color color = promoContainerBox.generateColorBasedOnHex()
+		then:
+			getHexColor(color) == orangeHexColour
+	}
+
+	def "Generates promo box based on hex colour"() {
+		when:
+			BufferedImage generatedPromoBox = promoContainerBox.getImage()
+		then:
+			getHexColor(generatedPromoBox, 1, 1) == orangeHexColour
+	}
+
+	def "Generates promo box based on fixed height"() {
+		when:
+			BufferedImage generatedPromoBox = promoContainerBox.getImage()
+		then:
+			generatedPromoBox.getHeight() == PromoContainerBox.HEIGHT
+	}
+
+	def "Generates promo box based on fixed width"() {
+		when:
+			BufferedImage generatedPromoBox = promoContainerBox.getImage()
+		then:
+			generatedPromoBox.getWidth() == PromoContainerBox.WIDTH
+	}
+
+	private getHexColor(BufferedImage bufferedImage, int x, int y) {
+		int rgbBit = bufferedImage.getRGB(x, y)
+		Color color = new Color(rgbBit)
+		getHexColor(color)
+	}
+
+	private String getHexColor(Color color) {
+		String hexColor = Integer.toHexString(color.getRGB() & 0xffffff);
+		if (hexColor.length() < 6) {
+			hexColor = "000000".substring(0, 6 - hexColor.length()) + hexColor
+		}
+		"#" + hexColor
+	}
+
 }
